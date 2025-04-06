5d7a417882a4d7523f39cd697cf1c256dacfd854
Tr11nk <Yah.gavrilov2003@gmail.com>
2025-04-06 16:34:51 +0300

    Adding functional for button add Also add some realisation for button delete

--- PassManager/ViewModels/MainViewModel.cs
+++ PassManager/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PassManager.Views;
+﻿using Microsoft.Data.Sqlite;
+using PassManager.Views;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -8,19 +9,27 @@ namespace PassManager.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+
     public string Header => "Манагер";
-
-    public ObservableCollection<Person> People { get; }
-
+    private Data _selectedData;
+    public Data SelectedData
+    {
+        get => _selectedData;
+        set
+        {
+            _selectedData = value;
+            OnPropertyChanged(nameof(SelectedData));
+        }
+    }
+    public ObservableCollection<Data> Datac { get; }
+ 
     public MainViewModel()
     {
-        var people = new List<Person>
-    {
-        new Person("Neil", "Armstrong","123"),
-        new Person("Neil", "Armstrong","123")
-        };
-        People = new ObservableCollection<Person>(people);
-        People.CollectionChanged += (s, e) => OnPropertyChanged(nameof(People));
+
+        Datac = new ObservableCollection<Data>();
+        Datac.Add(new Data("Neil", "Armstrong", "123"));    
+      // Datac = new ObservableCollection<Data>(Data);
+        Datac.CollectionChanged += (s, e) => OnPropertyChanged(nameof(Datac));
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
@@ -30,16 +39,16 @@ public class MainViewModel : ViewModelBase
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    public class Person
+    public class Data
     {
         public string Site { get; set; }
         public string Login { get; set; }
 
         public string Password { get; set; }
-        public Person(string firstName, string lastName, string password)
+        public Data(string site, string login, string password)
         {
-            Site = firstName;
-            Login = lastName;
+            Site = site;
+            Login = login;
             Password = password;
 
         }
